e748ba31d82d60ba880951bcfc9834d5613b219e
Latiasx <tunt68348@gmail.com>
2026-01-01 16:01:21 +0530

    Created DataBase engine and fixed minor errors

--- app/db.py
+++ app/db.py
@@ -6,9 +6,12 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-import datetime
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+class Bass(DeclarativeBase):
+    pass
 
 class Post(DeclarativeBase):
     __tablename__ = 'posts'
@@ -26,3 +29,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(DeclarativeBase.metadata.create_all)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
