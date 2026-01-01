99e56bed7ae795bb8adf21a67bd1245232d49e11
Latiasx <tunt68348@gmail.com>
2026-01-02 00:36:45 +0530

    Added image post feature

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
+from app.images import imagekit
+import shutil
+import os
+import uuid
+import tempfile
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,21 +17,73 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-text_posts = {1: {"title": "New Post","content": "Cool Test Post"}} 
 
-@app.get("/posts")
-def get_all_posts():
-    return text_posts
+#End point to upload a post
+@app.post('/upload')
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+):
+    temp_file_path = None
 
-@app.get("/posts/{id}")
-def get_post(id: int) -> PostResponse:
-    if id not in text_posts:
-        raise HTTPException(status_code=404,detail='Post not found')
+    try:
+        with tempfile.NamedTemporaryFile(delete=False,suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+
+            with open(temp_file_path, "rb") as file_to_upload:
+                upload_result = imagekit.files.upload(
+                    file=file_to_upload,
+                    file_name=file.filename,
+                    use_unique_file_name=True,
+                    tags=['backend-upload']
+                )
+
+        if upload_result.url:  # Check if upload was successful by verifying URL exists
+            post = Post(
+                caption=caption,
+                url=upload_result.url,
+                file_type= "video" if file.content_type.startswith("video/") else "image",
+                file_name=upload_result.name
+            )
+
+            session.add(post) #Added post to the DB session
+            await session.commit() #Committed to the DB
+            await session.refresh(post) #Refreshed the post, to hydrate the auto created components
+            return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
     
-    return text_posts[id]
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+    file.file.close()
 
-@app.post("/posts")
-def create_post(post: PostCreate) -> PostResponse:
-    new_post = {"title": post.title, "content": post.content}
-    text_posts[max(text_posts.keys())+1] = new_post
-    return new_post
+
+
+#End point to view posts
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    #'.execute' for querying the Data Base 
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+
+    return {"posts": posts_data}
+        
--- app/db.py
+++ app/db.py
@@ -6,14 +6,14 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-from datetime import datetime
+from datetime import datetime, timezone
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-class Bass(DeclarativeBase):
+class Base(DeclarativeBase):
     pass
 
-class Post(DeclarativeBase):
+class Post(Base):
     __tablename__ = 'posts'
 
     id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
@@ -21,14 +21,14 @@ class Post(DeclarativeBase):
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
